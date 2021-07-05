e4204823e83ec82dfb331d299416e12b5723f4aa
geekxz <13298176895@163.com>
2021-07-05 13:36:20 +0800

    初始化版本

---
+++ pages/about/about.js
@@ -0,0 +1,126 @@
+// pages/about/about.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    starCount: 780,
+    forksCount: 75620,
+    visitTotal: 152450,
+    imglist:['https://oss.geekxz.com/hey-ui-oss/geekxz_qrcode.jpg']
+  },
+  attached() {
+    // console.log("success")
+    let that = this;
+    wx.showLoading({
+      title: '数据加载中',
+      mask: true,
+    })
+    let i = 0;
+    numDH();
+    function numDH() {
+      if (i < 20) {
+        setTimeout(function () {
+          that.setData({
+            starCount: i,
+            forksCount: i,
+            visitTotal: i
+          })
+          i++
+          numDH();
+        }, 20)
+      } else {
+        that.setData({
+          starCount: that.coutNum(7800),
+          forksCount: that.coutNum(75620),
+          visitTotal: that.coutNum(152450)
+        })
+      }
+    }
+    wx.hideLoading()
+  },
+  coutNum(e) {
+    if (e > 1000 && e < 10000) {
+      e = (e / 1000).toFixed(1) + 'k'
+    }
+    if (e > 10000) {
+      e = (e / 10000).toFixed(1) + 'W'
+    }
+    return e
+  },
+  copyLink(e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.link,
+      success: res => {
+        wx.showToast({
+          title: '已复制',
+          duration: 1000,
+        })
+      }
+    })
+  },
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.imglist // 需要预览的图片http链接列表  
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.attached();
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
